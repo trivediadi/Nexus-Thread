03aef3cf820b1aadc45c7a41ea18f9b655782adf
trivediadi <trivediadi667@gmail.com>
2026-08-30 23:18:51 +0530

    Add UserSchema and UserResponseSchema definitions using Pydantic for user data validation

---
+++ backend/app/schemas/user.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel,EmailStr,ConfigDict
+from datetime import datetime
+class UserSchema(BaseModel):
+    name:str | None=None
+    username:str
+    email:EmailStr
+    password:str
+
+class UserResponseSchema(BaseModel):
+    id:int
+    name:str |None=None
+    username:str
+    email:EmailStr
+    is_active:bool
+    is_superuser:bool
+    created_at:datetime
+    updated_at:datetime
+
+    model_config=ConfigDict(from_attributes=True)
