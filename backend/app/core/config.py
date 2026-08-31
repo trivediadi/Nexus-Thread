7979aeb3cebfdb23a283df7f2d9e1906f0a5ce6c
trivediadi <trivediadi667@gmail.com>
2026-08-31 20:10:39 +0530

    Add missing configuration fields to Settings class for database and security

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -4,6 +4,8 @@ class Settings(BaseSettings):
     model_config=SettingsConfigDict(env_file=".env",extra="ignore")
     
     DB_CONNECTION:str
-
+    SECRET_KEY:str
+    ALGORITHM:str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int 
 
 settings=Settings()
