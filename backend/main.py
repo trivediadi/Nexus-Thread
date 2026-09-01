201612ec7e08425779f17d0bc368d126c0ab5872
trivediadi <trivediadi667@gmail.com>
2026-09-01 18:23:36 +0530

    Include post routes in FastAPI application

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.core.database import Base,engine
 from app.api.v1.auth import user_routes
+from app.api.v1.posts import post_routes
 @asynccontextmanager
 async def lifespan(app:FastAPI):
     async with engine.begin() as conn:
@@ -12,4 +13,4 @@ async def lifespan(app:FastAPI):
 app=FastAPI(title="Nexus Thread",lifespan=lifespan)
 
 app.include_router(user_routes)
-
+app.include_router(post_routes)
