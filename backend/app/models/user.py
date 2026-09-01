02a23782308c09299329c21a2d1cb7fd034f9672
trivediadi <trivediadi667@gmail.com>
2026-09-01 18:23:07 +0530

    Add relationship for posts in UserModel and adjust imports

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,14 +1,18 @@
-from sqlalchemy.orm import Mapped,mapped_column
-from sqlalchemy import String,Boolean,DateTime,func
+from sqlalchemy.orm import Mapped,mapped_column,relationship
+from sqlalchemy import String,Boolean,DateTime,func,INTEGER
 from datetime import datetime
+from typing import List,TYPE_CHECKING
 
 
 from app.core.database import Base
 
+if TYPE_CHECKING:
+    from app.models.post import PostModel
+
 class UserModel(Base):
     __tablename__="users"
 
-    id:Mapped[int]=mapped_column(primary_key=True,index=True)
+    id:Mapped[int]=mapped_column(INTEGER,primary_key=True,index=True)
     name:Mapped[str|None]=mapped_column(String(100),nullable=True)
     username:Mapped[str]=mapped_column(String(50),unique=True,index=True,nullable=False)
     email:Mapped[str]=mapped_column(String(255),unique=True,nullable=False)
@@ -19,4 +23,4 @@ class UserModel(Base):
 
     created_at:Mapped[datetime]=mapped_column(DateTime(timezone=True),server_default=func.now(),nullable=False)
     updated_at:Mapped[datetime]=mapped_column(DateTime(timezone=True),server_default=func.now(),onupdate=func.now(),nullable=False)
-
+    posts:Mapped[list["PostModel"]]=relationship("PostModel",back_populates="author",cascade="all, delete-orphan")
